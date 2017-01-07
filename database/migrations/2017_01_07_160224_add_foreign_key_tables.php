201b61f30d815d380b3b6ba31b0eb850138cfe55
fred <contact@champloo.fr>
2017-01-07 18:02:20 +0100

    add foreign key for relationship to tables

---
+++ database/migrations/2017_01_07_160224_add_foreign_key_tables.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('models', function (Blueprint $table){
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+
+        Schema::table('make_up_artists', function (Blueprint $table){
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+
+        Schema::table('hairdressers', function (Blueprint $table){
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+
+        Schema::create('user_picture', function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('picture_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->foreign('picture_id')->references('id')->on('pictures')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+        });
+
+        Schema::table('pictures', function (Blueprint $table){
+            $table->integer('gallery_id')->unsigned()->index();
+            $table->foreign('gallery_id')->references('id')->on('galleries')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_picture');
+        Schema::table('models', function (Blueprint $table){
+            $table->dropForeign('user_id');
+        });
+        Schema::table('make_up_artists', function (Blueprint $table){
+            $table->dropForeign('user_id');
+        });
+        Schema::table('hairdressers', function (Blueprint $table){
+            $table->dropForeign('user_id');
+        });
+        Schema::table('pictures', function (Blueprint $table){
+           $table->dropForeign('gallery_id');
+        });
+    }
+}
